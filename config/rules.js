a36d2b1d5bec180ed45cc1660235534ba337f4c0
Chizh273 <chizh273@gmail.com>
2017-10-31 15:27:38 +0200

    added .babelrc and .editorconfig

--- config/rules.js
+++ config/rules.js
@@ -3,20 +3,8 @@ module.exports = [
     test: /\.js$/,
     exclude: /node_modules/,
     use: [
-      {
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            'es2015', 'es2016', 'es2017',
-            'stage-0', 'stage-1', 'stage-2',
-            'stage-3'
-          ],
-          plugins: ['transform-runtime']
-        }
-      },
-      {
-        loader: 'eslint-loader'
-      }
+      {loader: 'babel-loader'},
+      {loader: 'eslint-loader'}
     ]
   },
   {
@@ -25,7 +13,7 @@ module.exports = [
       {
         loader: 'file-loader',
         options: {
-          outputPath: 'images/'
+          outputPath: 'assets/'
         }
       }
     ]
